dc1610dfe85b9da83c258799e9f304586c12d003
ansari-vazir <121504284+ansari-vazir@users.noreply.github.com>
2025-09-20 20:09:29 +0530

    Update google.js

--- auth/google.js
+++ auth/google.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/auth/google/callback",
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
